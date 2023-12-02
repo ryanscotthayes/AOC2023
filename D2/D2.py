5613e87aaded83e66c2cf1e839828f36646787f3
ryanscotthayes <ryanscotthayes@gmail.com>
2023-12-02 13:08:50 -0600

    Refactored D2 to clean up

--- D2/D2.py
+++ D2/D2.py
@@ -2,54 +2,34 @@ import re
 with open(r'./AOC2023/D2/D2.data') as f:
     lines = f.read().split('\n')
 
-processData1 = []
+processData1,data,failures,maximums,passes = [],[],[],[],[]
+
 for i in lines:
     processData1.append(i.split(':'))
 
-data = []
 for i in processData1:
     data.append([i[0],i[1].split(';')])
 
-nr_reds = 12
-nr_greens = 13
-nr_blues = 14
-
-failures = []
-maximums = []
 for i in range(0,len(data)):
-    max_red = 0
-    max_blue = 0
-    max_green = 0
+    redArray,blueArray,greenArray = [],[],[]
     for j in range(0,len(data[i][1])):
         redShown = re.findall(' [0-9]* red',data[i][1][j])
-        reds = sum([int(re.sub('[A-z ]','',i)) for i in redShown])
         blueShown = re.findall(' [0-9]* blue',data[i][1][j])
-        blues = sum([int(re.sub('[A-z ]','',i)) for i in blueShown])
         greenShown = re.findall(' [0-9]* green',data[i][1][j])
-        greens = sum([int(re.sub('[A-z ]','',i)) for i in greenShown])
-        if reds > max_red:
-            max_red = reds
-        if blues > max_blue:
-            max_blue = blues
-        if greens > max_green:
-            max_green = greens
-        if reds > nr_reds:
+        redArray.append(sum([int(re.sub('[A-z ]','',i)) for i in redShown]))
+        blueArray.append(sum([int(re.sub('[A-z ]','',i)) for i in blueShown]))
+        greenArray.append(sum([int(re.sub('[A-z ]','',i)) for i in greenShown]))
+        if sum([int(re.sub('[A-z ]','',i)) for i in redShown]) > 12:
             failures.append([data[i][0],'red'])
-        elif blues > nr_blues:
+        elif sum([int(re.sub('[A-z ]','',i)) for i in blueShown]) > 14:
             failures.append([data[i][0],'blue'])
-        elif greens > nr_greens:
+        elif sum([int(re.sub('[A-z ]','',i)) for i in greenShown]) > 13:
             failures.append([data[i][0],'green'])
-    maximums.append(max_red*max_blue*max_green)
+    maximums.append(max(redArray)*max(blueArray)*max(greenArray))
 
-passes = []
 for i in range(0,len(data)):
-    roundNumber = int(re.sub('[^0-9]*','',data[i][0]))
-    if roundNumber in [int(re.sub('[^0-9]*','',xyz[0]))for xyz in failures]:
-        pass # in the failure list
-    else:
-        passes.append(roundNumber)
+    if int(re.sub('[^0-9]*','',data[i][0])) not in [int(re.sub('[^0-9]*','',x[0]))for x in failures]:
+        passes.append(int(re.sub('[^0-9]*','',data[i][0]))) #append the round number if not in list of failures
 
 print('Round 1: '+ str(sum(passes)))
 print('Round 2: '+ str(sum(maximums)))
-
-
