46b3e9185944ff05996b9cd144c14bb92485380c
ryanscotthayes <ryanscotthayes@gmail.com>
2023-12-02 23:51:29 -0600

    Halfway through part 1

--- D3/D3.py
+++ D3/D3.py
@@ -1,3 +1,27 @@
-with open(r'./AOC2023/D3/D3.example') as f:
+with open(r'./AOC2023/D3/D3.data') as f:
     lines = f.read().split('\n')
 print(lines)
+indexOfSpecials = []
+for i in range(0,len(lines)): 
+    for j in range(0,len(lines[i])):
+        try:
+            int(lines[i][j])
+        except:
+            if lines[i][j] != '.':
+                indexOfSpecials.append([i,j])
+print(indexOfSpecials)
+#indexOfSpecials = indexOfSpecials[0]
+placesToLook = []
+for i in indexOfSpecials:
+    print(i)
+    placesToLook.append(str(i[0]+1)+','+str(i[1]))
+    placesToLook.append(str(i[0]+1)+','+str(i[1]+1))
+    placesToLook.append(str(i[0])+','+str(i[1]+1))
+    placesToLook.append(str(i[0]-1)+','+str(i[1]+1))
+    placesToLook.append(str(i[0]-1)+','+str(i[1]))
+    placesToLook.append(str(i[0]-1)+','+str(i[1]-1))
+    placesToLook.append(str(i[0])+','+str(i[1]-1))
+    placesToLook.append(str(i[0]+1)+','+str(i[1]-1))
+placesToLook = list(set(placesToLook))
+print('Finished Finding the places to look for integers')
+print(len(placesToLook))
