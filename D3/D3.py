4f70381c5b1bc4e5e49656b6ac2aadd9821fb55b
ryanscotthayes <ryanscotthayes@gmail.com>
2023-12-03 00:28:59 -0600

    Part 1 Done

--- D3/D3.py
+++ D3/D3.py
@@ -1,6 +1,6 @@
+import time
 with open(r'./AOC2023/D3/D3.data') as f:
     lines = f.read().split('\n')
-print(lines)
 indexOfSpecials = []
 for i in range(0,len(lines)): 
     for j in range(0,len(lines[i])):
@@ -9,11 +9,9 @@ for i in range(0,len(lines)):
         except:
             if lines[i][j] != '.':
                 indexOfSpecials.append([i,j])
-print(indexOfSpecials)
 #indexOfSpecials = indexOfSpecials[0]
 placesToLook = []
 for i in indexOfSpecials:
-    print(i)
     placesToLook.append(str(i[0]+1)+','+str(i[1]))
     placesToLook.append(str(i[0]+1)+','+str(i[1]+1))
     placesToLook.append(str(i[0])+','+str(i[1]+1))
@@ -23,5 +21,40 @@ for i in indexOfSpecials:
     placesToLook.append(str(i[0])+','+str(i[1]-1))
     placesToLook.append(str(i[0]+1)+','+str(i[1]-1))
 placesToLook = list(set(placesToLook))
-print('Finished Finding the places to look for integers')
-print(len(placesToLook))
+surroundSpecials = []
+for i in placesToLook:
+    surroundSpecials.append([int(x) for x in i.split(",")])
+
+enginePartLocations = []
+for i in surroundSpecials:
+    try: #check if integer
+        int(lines[i[0]][i[1]])
+        enginePartLocations.append([i[0],i[1]])
+    except:
+        pass
+
+engineLocationStartsString = []
+for i in enginePartLocations:
+    currentPos = [i[0],i[1]]
+    currentCheck = lines[i[0]][i[1]]
+    while currentCheck.isdigit():
+        currentPos = [currentPos[0],currentPos[1]-1]
+        currentCheck = lines[currentPos[0]][currentPos[1]]
+    engineLocationStartsString.append(str(currentPos[0])+','+str(currentPos[1]+1))
+
+engineLocationStartsString = list(set(engineLocationStartsString))
+engineLocationStarts = []
+for i in engineLocationStartsString:
+    engineLocationStarts.append([int(x) for x in i.split(",")])
+sumPartNums = 0
+for i in engineLocationStarts:
+    string = lines[i[0]][i[1]:i[1]+3]
+    #print(string)
+    partNumberString = ''
+    for char in string:
+        if char.isdigit():
+            partNumberString = partNumberString + char
+        else:
+            break
+    sumPartNums += int(partNumberString)
+print(sumPartNums)
