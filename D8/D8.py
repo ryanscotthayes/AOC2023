9bd4237716a083e46d072bab1993d6d20bafca6d
ryanscotthayes <ryanscotthayes@gmail.com>
2023-12-09 00:15:03 -0600

    D1 Complete

--- D8/D8.py
+++ D8/D8.py
@@ -1,2 +1,30 @@
-with open(r'./AOC2023/D7/D7.data') as f:
+import re
+with open(r'./AOC2023/D8/D8.data') as f:
     lines = f.read().split('\n')
+instructions = list(lines[0])
+rawmap = [re.sub('[\(\)]','',x).split(' = ') for x in lines[2:]]
+map = []
+for i in rawmap:
+    map.append([i[0],i[1].split(', ')])
+
+
+def search(instList,map,currentPos = 'AAA'):
+    counter = 0
+    while True:
+        for i in range(0,len(instList)):
+            if currentPos =='ZZZ':
+                return counter
+            counter+=1
+            for j in range(0,len(map)):
+                if currentPos == map[j][0]:
+                    options = map[j][1]
+            if instList[i] == 'R':
+                currentPos = options[1]
+            elif instList[i] == 'L':
+                currentPos = options[0]
+                     
+        if currentPos =='ZZZ':
+                return counter
+
+
+print('Part 1: '+ str(search(instructions,map,'AAA')))
