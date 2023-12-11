0d652d9c3657898c6390257d0d12ed0f121980de
ryanscotthayes <ryanscotthayes@gmail.com>
2023-12-11 00:34:41 -0600

    D9 Finished

--- D9/D9.py
+++ D9/D9.py
@@ -1,2 +1,24 @@
-with open(r'./AOC2023/D8/D8.data') as f:
-    lines = f.read().split('\n')
+import numpy as np
+#from numpy.polynomial.polynomial import Polynomial
+
+with open(r'./AOC2023/D9/D9.data') as f:
+    lines = [x.split(' ') for x in f.read().split('\n')]
+
+data = []
+for i in lines:
+    intermed = []
+    for j in i:
+        intermed.append(int(j))
+    data.append(intermed)
+
+
+answer,answer2 = 0,0
+answerArray = []
+for item in data:
+    poly = np.polynomial.polynimial.Polynomial.fit(np.arange(len(item)),item,deg=len(item)-1)
+    answerArray.append([round(poly(len(item))),round(poly(-1))])
+    answer += round(poly(len(item)))
+    answer2 += round(poly(-1))
+
+print('Part 1: '+ str(answer))
+print('Part 2: '+ str(answer2))
